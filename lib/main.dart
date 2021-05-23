99427e6c371e41e83cedd05173bc5fa5943d4c31
Christopher Fujino <christopherfujino@gmail.com>
2021-05-23 15:48:20 -0700

    Revert "Revert "port from touch UI to gamepad UI""
    This reverts commit c01cbb08dfdbe9f1ea18b81f7e748c113abcc17f.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
+import 'dart:io' as io;
+
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/flame.dart';
-import 'package:flutter/foundation.dart';
 
 import './game.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (!kIsWeb) {
-    await Flame.device.setPortrait();
-    await Flame.device.fullScreen();
+  if (!io.Platform.isLinux) {
+    throw Exception('This game only supports Linux');
   }
+  await Flame.device.setPortrait();
+  await Flame.device.fullScreen();
   runApp(GameWidget(game: SpaceShooterGame()));
 }

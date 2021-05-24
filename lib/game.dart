a79463c16d06f6e1e1f4a769c01645f9f1b99ae6
Christopher Fujino <christopherfujino@gmail.com>
2021-05-23 19:44:24 -0700

    only check for gamepad if --dart-define=USES_GAMEPAD=TRUE

--- lib/game.dart
+++ lib/game.dart
@@ -26,7 +26,10 @@ enum YAxis {
 }
 
 class SpaceShooterGame extends BaseGame with HasCollidables {
+  SpaceShooterGame()
+      : usesGamepad = const String.fromEnvironment('USES_GAMEPAD') == 'TRUE';
 
+  final bool usesGamepad;
   PlayerComponent? player;
 
   int score = 0;
@@ -45,7 +48,11 @@ class SpaceShooterGame extends BaseGame with HasCollidables {
 
     add(ScoreComponent());
 
-    joystickFileDescriptor = openFD();
+    if (usesGamepad) {
+      joystickFileDescriptor = openFD();
+    } else {
+      joystickFileDescriptor = -1;
+    }
   }
 
   static XAxis? currentX; // `null` means not currently pressed
@@ -80,66 +87,69 @@ class SpaceShooterGame extends BaseGame with HasCollidables {
         player?.move(0, distance * dt);
         break;
     }
-    final ffi.Pointer<JSEvent> event = flushFD(joystickFileDescriptor);
-    if (event != ffi.nullptr) {
-      switch (event.ref.type) {
-        case JS_EVENT_BUTTON:
-          switch (event.ref.number) {
-            // A or B buttons mean fire
-            case 0:
-            case 1:
-              if (event.ref.value == 1) {
-                player?.beginFire();
-              } else {
-                player?.stopFire();
-              }
-              break;
-            case 2: // X
-              break;
-            case 3: // Y
-              break;
-            case 4: // L
-              break;
-            case 5: // R
-              break;
-            case 6: // Select
-              break;
-            case 7: // Start
-              break;
-            default:
-              // TODO just ignore these
-              throw Exception("Oops! Unsupported button: ${event.ref.number}");
-          }
-          break;
-        case JS_EVENT_AXIS:
-          switch (event.ref.number) {
-            case 6: // X-axis
-              if (event.ref.value > 0) {
-                currentX = XAxis.Right;
-              } else if (event.ref.value < 0) {
-                currentX = XAxis.Left;
-              } else {
-                currentX = null;
-              }
-              break;
-            case 7: // Y-axis
-              if (event.ref.value > 0) {
-                currentY = YAxis.Down;
-              } else if (event.ref.value < 0) {
-                currentY = YAxis.Up;
-              } else {
-                currentY = null;
-              }
-              break;
-            default:
-              throw Exception('Unimplemented joystick axis ${event.ref.number}');
-          }
-          break;
-        case JS_EVENT_INIT:
-          // These should be discarded by the C code
-          throw Exception('Received unexpected JS_EVENT_INIT event');
-        default:
-          throw Exception('Unknown event type ${event.ref.type}');
+    if (usesGamepad) {
+      final ffi.Pointer<JSEvent> event = flushFD(joystickFileDescriptor);
+      if (event != ffi.nullptr) {
+        switch (event.ref.type) {
+          case JS_EVENT_BUTTON:
+            switch (event.ref.number) {
+              // A or B buttons mean fire
+              case 0:
+              case 1:
+                if (event.ref.value == 1) {
+                  player?.beginFire();
+                } else {
+                  player?.stopFire();
+                }
+                break;
+              case 2: // X
+                break;
+              case 3: // Y
+                break;
+              case 4: // L
+                break;
+              case 5: // R
+                break;
+              case 6: // Select
+                break;
+              case 7: // Start
+                break;
+              default:
+                // TODO just ignore these
+                throw Exception("Oops! Unsupported button: ${event.ref.number}");
+            }
+            break;
+          case JS_EVENT_AXIS:
+            switch (event.ref.number) {
+              case 6: // X-axis
+                if (event.ref.value > 0) {
+                  currentX = XAxis.Right;
+                } else if (event.ref.value < 0) {
+                  currentX = XAxis.Left;
+                } else {
+                  currentX = null;
+                }
+                break;
+              case 7: // Y-axis
+                if (event.ref.value > 0) {
+                  currentY = YAxis.Down;
+                } else if (event.ref.value < 0) {
+                  currentY = YAxis.Up;
+                } else {
+                  currentY = null;
+                }
+                break;
+              default:
+                throw Exception(
+                    'Unimplemented joystick axis ${event.ref.number}');
+            }
+            break;
+          case JS_EVENT_INIT:
+            // These should be discarded by the C code
+            throw Exception('Received unexpected JS_EVENT_INIT event');
+          default:
+            throw Exception('Unknown event type ${event.ref.type}');
+        }
       }
     }
   }
@@ -149,7 +159,8 @@ class SpaceShooterGame extends BaseGame with HasCollidables {
     super.render(canvas);
 
     debugTextconfig.render(canvas, fps(120).toString(), Vector2(0, 50));
-    debugTextconfig.render(canvas, 'Objects: ${components.length}', Vector2(0, 100));
+    debugTextconfig.render(
+        canvas, 'Objects: ${components.length}', Vector2(0, 100));
   }
 
   void increaseScore() {

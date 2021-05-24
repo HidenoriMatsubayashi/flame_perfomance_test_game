90a994efe317d8f105b9a4a71cc6d34e30e9257c
Christopher Fujino <christopherfujino@gmail.com>
2021-05-23 19:42:10 -0700

    fix explosion looping forever

--- lib/components/explosion_component.dart
+++ lib/components/explosion_component.dart
@@ -3,7 +3,6 @@ import 'package:flame/components.dart';
 import '../game.dart';
 
 class ExplosionComponent extends SpriteAnimationComponent with HasGameRef<SpaceShooterGame> {
-
   ExplosionComponent(double x, double y): super(
       position: Vector2(x, y),
       size: Vector2.all(50),
@@ -17,6 +16,6 @@ class ExplosionComponent extends SpriteAnimationComponent with HasGameRef<SpaceS
       amount: 6,
       textureSize: Vector2.all(32),
     ));
+    animation!.loop = false;
   }
 }
-
